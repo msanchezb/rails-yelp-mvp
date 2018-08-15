74d354301c87b420b10827b5f3487f78969ebbc6
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-15 15:15:26 +0100

    rake tests passed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,9 +9,10 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_reviews_path(@restaurant)
+      redirect_to restaurant_path(@restaurant)
     else
-      render "restaurants/show"
+      render :new
+      # render "restaurants/show"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :new, :show, :create] do
-    resources :reviews, only: [:index, :create]
+    resources :reviews, only: [:index, :create, :new]
   end
 
   namespace :admin do
